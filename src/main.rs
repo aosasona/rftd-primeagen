145cdb4d6bab1cf33d0f89d71d77ef29edf05325
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-05-07 19:14:05 +0100

    manual collect

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 fn main() {
-    let foo: Vec<_> = vec![1, 2, 3].iter().map(|x| x + 1).collect();
-    println!("{:?}", foo);
+    let nums: Vec<_> = vec![1, 2, 3].iter().map(|x| x + 1).collect();
+    println!("{:?}", nums);
 
     // manual collect implementation
     let data = vec![1, 2, 3]; // data will live until main returns
-    let mut foo2 = data.iter().map(|x| x + 1);
-    let mut new_foo2 = vec![];
+    let mut nums2 = data.iter().map(|x| x + 1);
+    let mut new_nums2 = vec![];
 
-    while let Some(x) = foo2.next() {
-        new_foo2.push(x)
+    while let Some(x) = nums2.next() {
+        new_nums2.push(x)
     }
+
+    // using collect to join strings
+    let str_collect: String = ["hello", ", ", "world", "!"].into_iter().collect();
+    println!("{}", str_collect);
 }
