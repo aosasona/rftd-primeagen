9d1a8a745ddbc246dc4aecd6d71123da0cee010d
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-05-07 19:11:57 +0100

    manual collect

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
     let foo: Vec<_> = vec![1, 2, 3].iter().map(|x| x + 1).collect();
     println!("{:?}", foo);
+
+    // manual collect implementation
+    let data = vec![1, 2, 3]; // data will live until main returns
+    let mut foo2 = data.iter().map(|x| x + 1);
+    let mut new_foo2 = vec![];
+
+    while let Some(x) = foo2.next() {
+        new_foo2.push(x)
+    }
 }
