c5e9459fde98b809e3bbcaa9b91841113ca2fc5d
Replit user <>
2023-05-16 10:42:31 +0000

    added frargs

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,42 @@
 import fs from "fs";
+import path from "path";
 
-const foo = [1, 2, 3];
-for (let i = 0; i < foo.length; i++) {
-  foo[i] += 1;
+// const foo = [1, 2, 3];
+// for (let i = 0; i < foo.length; i++) {
+//   foo[i] += 1;
+// }
+// console.log(foo);
+
+// // alternatively using map
+// const foo2 = [1, 2, 3].map((x) => x + 1);
+// console.log(foo2);
+
+// fs.readFileSync("lines")
+//   .toString()
+//   .split("\n")
+//   .filter((_, idx) => idx % 2 == 0)
+//   .filter((_, idx) => idx > 1 && idx < 4)
+//   .forEach((line) => {
+//     console.log(line);
+//   });
+
+
+// file reader via args
+function frargs() {
+  try {
+    const target = process.argv[2];
+    if(!target) return console.error('no file specified');
+    const filePath = path.join(__dirname, "..", target);
+    if(!fs.existsSync(filePath)) return console.error('specified file does not exist');
+    fs.readFileSync(filePath).toString().split('\n').forEach((line) => {
+      if(isNaN(parseInt(line.trim()))) {
+       return console.log('line is not a number');
+      }
+      console.log(line);
+    });
+  } catch(e) {
+    console.error("error: " + (e as { message: string }).message)
+  }
 }
-console.log(foo);
 
-// alternatively using map
-const foo2 = [1, 2, 3].map((x) => x + 1);
-console.log(foo2);
-
-fs.readFileSync("lines")
-  .toString()
-  .split("\n")
-  .filter((_, idx) => idx % 2 == 0)
-  .filter((_, idx) => idx > 1 && idx < 4)
-  .forEach((line) => {
-    console.log(line);
-  });
+frargs();
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,48 @@
-use std::{self, fs::read_to_string};
+// use std::{self, fs::read_to_string};
 
-fn iterator() {
-    let nums: Vec<_> = vec![1, 2, 3].iter().map(|x| x + 1).collect();
-    println!("{:?}", nums);
+// fn iterator() {
+//     let nums: Vec<_> = vec![1, 2, 3].iter().map(|x| x + 1).collect();
+//     println!("{:?}", nums);
 
-    // manual collect implementation
-    let data = vec![1, 2, 3]; // data will live until main returns
-    let mut nums2 = data.iter().map(|x| x + 1);
-    let mut new_nums2 = vec![];
+//     // manual collect implementation
+//     let data = vec![1, 2, 3]; // data will live until main returns
+//     let mut nums2 = data.iter().map(|x| x + 1);
+//     let mut new_nums2 = vec![];
 
-    while let Some(x) = nums2.next() {
-        new_nums2.push(x)
-    }
+//     while let Some(x) = nums2.next() {
+//         new_nums2.push(x)
+//     }
 
-    // using collect to join strings
-    let str_collect: String = ["hello", ", ", "world", "!"].into_iter().collect();
-    println!("{}", str_collect);
-}
+//     // using collect to join strings
+//     let str_collect: String = ["hello", ", ", "world", "!"].into_iter().collect();
+//     println!("{}", str_collect);
+// }
 
-fn read_file() {
-    // read line and skip the even lines
-    let file = read_to_string("lines").unwrap();
-    file.lines()
-        .enumerate()
-        .filter(|(idx, _)| idx % 2 == 0)
-        .skip(2)
-        .take(2)
-        .for_each(|(_, line)| println!("{}", line))
+// fn read_file() {
+//     // read line and skip the even lines
+//     let file = read_to_string("lines").unwrap();
+//     file.lines()
+//         .enumerate()
+//         .filter(|(idx, _)| idx % 2 == 0)
+//         .skip(2)
+//         .take(2)
+//         .for_each(|(_, line)| println!("{}", line))
+// }
+
+fn frargs() {
+    let target: String = std::env::args().nth(1).expect("no filename provided");
+    let file = std::fs::read_to_string(target).expect("unable to read file");
+    file.lines().for_each(|line| {
+        if let Ok(value) = line.parse::<usize>() {
+            println!("{}", value);
+        } else {
+            println!("line is not a number");
+        }
+    });
 }
 
 fn main() {
-    iterator();
-    read_file();
+    // iterator();
+    // read_file();
+    frargs();
 }
